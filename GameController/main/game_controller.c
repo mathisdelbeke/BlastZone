7926330c879519e2cee9741a989e01d1225e68ea
mathisdelbeke <mathisdelbeke.2003@hotmail.com>
2025-07-13 14:59:08 +0200

    Reading MPU6500 data over I2C and sending it over UART to COM port

--- GameController/main/game_controller.c
+++ GameController/main/game_controller.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <string.h>
-#include <driver/uart.h>
 #include <driver/gpio.h>
+
+#include <driver/uart.h>
+#include "driver/i2c.h"
+
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+
+#define UPDATE_CONTROLLER_DELAY 16                              // Game refreshes with 60 hz
+
 #define UART_NUM UART_NUM_0                                     // UART 0 hard connected to USB-UART bridge
 #define UART_BUF_SIZE 1024
+#define UART_BAUD_RATE 9600
+
+#define I2C_MASTER_NUM      I2C_NUM_0                           
+#define I2C_MASTER_SDA_IO   21                                  
+#define I2C_MASTER_SCL_IO   22                                  
+#define I2C_MASTER_FREQ_HZ  100000                              
+#define I2C_MASTER_TX_BUF_DISABLE 0
+#define I2C_MASTER_RX_BUF_DISABLE 0
+#define MPU6500_I2C_ADDRESS 0x68
+#define MPU6500_START_REG 0x3B
+#define MPU6500_BYTES_TO_READ 14
 
 static void init_uart();
-static void send_controller_pos();
+static void init_i2c_master();
+static void wake_up_mpu6500();
+static void read_mpu6500_data(uint8_t *controller_pos);
 static void update_controller_pos(void *pvParameters);
 
-void init_uart() {
+static void init_uart() {
     uart_config_t uart_config = {
-        .baud_rate = 9600,          
+        .baud_rate = UART_BAUD_RATE,          
         .data_bits = UART_DATA_8_BITS,
         .parity    = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
@@ -22,24 +41,56 @@ void init_uart() {
     };
     uart_param_config(UART_NUM, &uart_config);
     uart_driver_install(UART_NUM, UART_BUF_SIZE * 2, 0, 0, NULL, 0);
+    printf("\n");
 }
 
-void update_controller_pos(void *pvParameters) {
+static void init_i2c_master() {
+    esp_err_t err;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ
+    };
+    err = i2c_param_config(I2C_MASTER_NUM, &conf);
+    if (err == ESP_OK) {
+        err = i2c_driver_install(I2C_MASTER_NUM, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+        if (err != ESP_OK) printf("(E) i2c driver install: %s\n", esp_err_to_name(err));
+    }
+    else printf("(E) i2c param config: %s\n", esp_err_to_name(err));
+}
+
+static void wake_up_mpu6500() {
+    uint8_t wake_cmd[2] = {0x6B, 0x00};  // PWR_MGMT_1 = 0
+    esp_err_t err = i2c_master_write_to_device(I2C_MASTER_NUM, MPU6500_I2C_ADDRESS, wake_cmd, 2, pdMS_TO_TICKS(100));
+    if (err != ESP_OK) printf("(E) i2c wake mpu6500: %s\n", esp_err_to_name(err));
+}
+
+static void update_controller_pos(void *pvParameters) {
     for (;;) {
-        // read sensor
-        send_controller_pos();
-        vTaskDelay(pdMS_TO_TICKS(16));                          // Game refreshes with 60 hz
+        uint8_t controller_pos[MPU6500_BYTES_TO_READ];
+        read_mpu6500_data(&controller_pos);
+        uart_write_bytes(UART_NUM, controller_pos, MPU6500_BYTES_TO_READ);
+        vTaskDelay(pdMS_TO_TICKS(1000)); 
+        vTaskDelay(pdMS_TO_TICKS(UPDATE_CONTROLLER_DELAY));                        
     }
 }
 
-void send_controller_pos() {
-    // The string to send
-    const char *test_str = "Hello COM5 from ESP32!\n";
-    uart_write_bytes(UART_NUM, test_str, strlen(test_str));
-    vTaskDelay(pdMS_TO_TICKS(1000)); 
+static void read_mpu6500_data(uint8_t *controller_pos) {
+    esp_err_t err;
+    uint8_t reg = MPU6500_START_REG;
+    err = i2c_master_write_to_device(I2C_MASTER_NUM, MPU6500_I2C_ADDRESS, &reg, 1, pdMS_TO_TICKS(1000));
+    if (err != ESP_OK) printf("(E) i2c write mpu6500: %s/n", esp_err_to_name(err));
+    
+    err = i2c_master_read_from_device(I2C_MASTER_NUM, MPU6500_I2C_ADDRESS, controller_pos, MPU6500_BYTES_TO_READ, pdMS_TO_TICKS(1000));
+    if (err != ESP_OK) printf("(E) i2c read mpu6500: %s/n", esp_err_to_name(err));
 }
 
 void app_main(void) {
     init_uart();
-    xTaskCreate(update_controller_pos, "update_controller_pos", 2048, NULL, 5, NULL);
+    init_i2c_master();
+    wake_up_mpu6500();
+    xTaskCreate(update_controller_pos, "update_controller_pos", 4096, NULL, 5, NULL);
 }
